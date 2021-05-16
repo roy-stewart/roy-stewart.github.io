e18baf7c73b0018cf30442a54fc9f76420b013f8
Roy Stewart <roythomasstewart@gmail.com>
2021-05-16 06:28:53 -0400

    Improved the aria attribute usage on  the navigation menu

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function onNavButtonClicked() {
     const navMenu = document.getElementById('site-nav-menu');
     navMenu.classList.toggle('shown');
+    const isHidden = !navMenu.classList.contains('shown');
+    navMenu.setAttribute('aria-hidden', isHidden);
 }
